6ade11d92916204b360c5dd5f5918ddd4be7d865
Dossa Rousslan Fernand Julien <dosssman@hotmail.fr>
2019-02-15 14:54:26 +0900

    Debuging manual backprop

--- manual-backprop.py
+++ manual-backprop.py
@@ -1,13 +1,13 @@
 import numpy as np
 import tensorflow as tf
-import tf.zeros_initializer as zinit
+from tensorflow import zeros_initializer as zinit
 from tensorflow.keras.datasets.mnist import load_data
 from tensorflow.contrib.layers import xavier_initializer as xinit
 import matplotlib.pyplot as plt
 (x_train, y_train), (x_test, y_test) = load_data()
 
 n_x = 784
-n_h = 100
+n_z = 100
 n_y = 10
 
 x_train = np.reshape( x_train, [len(x_train), 784 ])
@@ -25,7 +25,7 @@ class NN(object):
         self.x = tf.placeholder( dtype=tf.float32, shape=[None, 784])
         self.y = tf.placeholder( dtype=tf.float32, shape=[None, 10])
 
-        with variable_scope( "weights"):
+        with tf.variable_scope( "weights"):
             self.w0 = tf.get_variable( shape=[n_x, n_h], initializer=xinit(),
                 name="w0")
             self.b0 = tf.get_variable( shape=[n_h], initializer=zinit(),
@@ -36,28 +36,44 @@ class NN(object):
                 name="b_out")
 
         # Init
+        self.create_layers()
+        self.create_backprop()
+        self.create_gradient_step()
+
+        # TF init
         self.sess = tf.Session()
         init = tf.global_variables_initializer()
-        self.run( init)
+        self.sess.run( init)
 
-        self.cost = tf.square( tf.substract( self.output, self.y))
+        # self.cost = tf.square( tf.subtract( selfoutput, self.y))
 
-    def create_layers( self, x):
-        with variable_scope( "layers"):
-            self.z0 = tf.add( tf.multiply( self.x, self.w0), self.b0)
+    def create_layers( self):
+        with tf.variable_scope( "layers"):
+            self.z0 = tf.add( tf.matmul( self.x, self.w0), self.b0)
             self.l0 = tf.sigmoid( self.z0, name="l0")
 
-            self.z_output = tf.add( tf.multiply( self.l0, self.w1), self.b1)
+            self.z_output = tf.add( tf.matmul( self.l0, self.w_out), self.b_out)
             self.output = tf.sigmoid( self.z_output)
 
         return self.output
 
-    def backprop_pass( self, x, y): #???
-        with variable_scope("deltas"):
-            self.d_z_out = tf.multiply((self.output - y),
+    def create_backprop( self): #???
+        with tf.variable_scope("deltas"):
+            print( self.output)
+            print( self.y)
+            input()
+            
+            diff = tf.subtract( self.output, self.y)
+            print(diff)
+            print( self.z_output)
+            print( self.sigmoid_prime(self.z_output))
+            input()
+
+            self.d_z_out = tf.multiply( diff,
                 self.sigmoid_prime( self.z_output), name="d_z_out")
             self.d_b_out = selfa.d_z_out
-            self.d_w_out = t.matmul( tf.transpose( self.l0), self.d_z_out)
+            self.d_w_out = t.matmul( tf.transpose( self.l0),
+                self.d_z_out, name="d_w_out")
 
             self.d_l0 = tf.matmul( self.d_z_out, tf.transpose( self.w_out))
             self.d_z0 = tf.multiply( self.d_l0, self.sigmoid_prime( self.z0))
@@ -66,34 +82,36 @@ class NN(object):
 
     def create_gradient_step( self, lr=0.1):
         self.step = [
-            tf.assign( self.w_out, tf.substract( self.w_out,
+            tf.assign( self.w_out, tf.subtract( self.w_out,
                 tf.multiply( lr, self.d_w_out))),
-            tf.assign( self.b_out, tf.substract( self.b_out,
+            tf.assign( self.b_out, tf.subtract( self.b_out,
                 tf.multiply( lr, self.d_b_out))),
-            tf.assign( self.b0, tf.substract( self.b0,
+            tf.assign( self.b0, tf.subtract( self.b0,
                 tf.multiply( lr, self.d_b0))),
-            tf.assign( self.w0, tf.substract( self.w0,
+            tf.assign( self.w0, tf.subtract( self.w0,
                 tf.multiply( lr, self.d_w0)))
         ]
         # Session run etc ...
 
-    def train( self, x_train, y_train, lr=.1, batch_size=100):
-        with tf.get_default_session() as sess:
-            acct_mat = tf.equal( tf.argmax( [.output, 1), tf.argmax( y, 1))
+    def train( self, x_train, y_train, lr=.1, batch_size=100, max_epoch=10000):
+        with self.sess as sess:
+            acct_mat = tf.equal( tf.argmax( self.output, 1), tf.argmax( self.y, 1))
             acct_res = tf.reduce_sum( tf.cast( acct_mat, tf.float32))
 
-            acct_res = tf.equal( tf.argmax( self.fwd_pass( x), )
-            for epoch in range(10000):
+            for epoch in range(max_epoch):
                 batch_start = 0
-                for batch in range( len(x_train) % batch_size):
+
+                for _ in range(int(len(x_train) / batch_size)):
                     batch_x, batch_y = x_train[batch_start:(batch_start+batch_size)], \
                         y_train[batch_start:(batch_start+batch_size)]
 
-                    sess.run( self.step, feed_dict={ self.x: batch_x, self.y: batchv_y})
+                    sess.run( self.step, feed_dict={ self.x: batch_x, self.y: batch_y})
+                    batch_start += batch_size
 
                     if epoch % 1000 == 0:
-                        res = sess.run( acct_res,feed_dict={ output = x_test[:1000],
-                            y=y_test[:1000]})
+                        res = sess.run( acct_res,feed_dict={ self.x: x_test[:1000],
+                            self.y: y_test[:1000]})
+
                         print(res)
 
     def sigmoid( self, x):
@@ -102,4 +120,9 @@ class NN(object):
 
     def sigmoid_prime( self, x):
         return tf.multiply( self.sigmoid(x),
-            tf.substract( tf.constant(1.0), x))
+            tf.subtract( tf.constant(1.0), x))
+
+if __name__ == "__main__":
+
+    nn = NN( n_x, n_y, n_z)
+    nn.train( x_train, y_train)
