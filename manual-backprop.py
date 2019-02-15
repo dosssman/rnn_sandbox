abe1f313b3c414fb585d289f3530db3edc1e9b81
z3r0 <dosssman@hotmail.fr>
2019-02-15 09:47:39 +0900

    Impled to Acct Test

--- manual-backprop.py
+++ manual-backprop.py
@@ -42,7 +42,7 @@ class NN(object):
 
         self.cost = tf.square( tf.substract( self.output, self.y))
 
-    def fwd_pass( self, x):
+    def create_layers( self, x):
         with variable_scope( "layers"):
             self.z0 = tf.add( tf.multiply( self.x, self.w0), self.b0)
             self.l0 = tf.sigmoid( self.z0, name="l0")
@@ -52,11 +52,11 @@ class NN(object):
 
         return self.output
 
-    def backprop( self, x, y): #???
+    def backprop_pass( self, x, y): #???
         with variable_scope("deltas"):
             self.d_z_out = tf.multiply((self.output - y),
                 self.sigmoid_prime( self.z_output), name="d_z_out")
-            self.d_b_out = self.d_z_out
+            self.d_b_out = selfa.d_z_out
             self.d_w_out = t.matmul( tf.transpose( self.l0), self.d_z_out)
 
             self.d_l0 = tf.matmul( self.d_z_out, tf.transpose( self.w_out))
@@ -64,8 +64,37 @@ class NN(object):
             self.d_b0 = self.z0
             self.d_w0 = tf.matmul( tf.transpose( self.x), self.d_z0)
 
-    def apply_gradient( self, lr=0.1):
-        self.d_b_out = 
+    def create_gradient_step( self, lr=0.1):
+        self.step = [
+            tf.assign( self.w_out, tf.substract( self.w_out,
+                tf.multiply( lr, self.d_w_out))),
+            tf.assign( self.b_out, tf.substract( self.b_out,
+                tf.multiply( lr, self.d_b_out))),
+            tf.assign( self.b0, tf.substract( self.b0,
+                tf.multiply( lr, self.d_b0))),
+            tf.assign( self.w0, tf.substract( self.w0,
+                tf.multiply( lr, self.d_w0)))
+        ]
+        # Session run etc ...
+
+    def train( self, x_train, y_train, lr=.1, batch_size=100):
+        with tf.get_default_session() as sess:
+            acct_mat = tf.equal( tf.argmax( [.output, 1), tf.argmax( y, 1))
+            acct_res = tf.reduce_sum( tf.cast( acct_mat, tf.float32))
+
+            acct_res = tf.equal( tf.argmax( self.fwd_pass( x), )
+            for epoch in range(10000):
+                batch_start = 0
+                for batch in range( len(x_train) % batch_size):
+                    batch_x, batch_y = x_train[batch_start:(batch_start+batch_size)], \
+                        y_train[batch_start:(batch_start+batch_size)]
+
+                    sess.run( self.step, feed_dict={ self.x: batch_x, self.y: batchv_y})
+
+                    if epoch % 1000 == 0:
+                        res = sess.run( acct_res,feed_dict={ output = x_test[:1000],
+                            y=y_test[:1000]})
+                        print(res)
 
     def sigmoid( self, x):
         return tf.div( tf.constant(1.0),
